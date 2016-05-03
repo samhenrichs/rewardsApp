d7d8ebfab12b437ae1bf837418b191d15a1fd4f7
samhenrichs <eurostepedits@gmail.com>
2016-05-03 18:34:05 -0500

    Tried to fix mobile

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,30 +1,53 @@
 class BlogsController < ApplicationController
 
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def new
     @blog = Blog.new
   end
 
   def create
     @blog = Blog.new(blog_params)
-    @blog.save
-    redirect_to @blog
+
+    if @blog.save
+      redirect_to @blog
+    else
+      render 'new'
+    end
   end
 
   def show
-    @blog = Blog.find(params[:id])
+
   end
 
   def edit
+
+  end
+
+  def update
+
+    if @blog.update(blog_params)
+      redirect_to @blog
+    else
+      render 'edit'
+    end
+
   end
 
   def destroy
+    @blog.destroy
+    redirect_to root_path
   end
 
   def index
-    @blogs = Blog.all
+    @blogs = Blog.all.order(created_at: :desc)
   end
 
   private
+
+    def find_post
+      @blog = Blog.find(params[:id])
+    end
 
     def blog_params
       params.require(:blog).permit(:title, :body, :image)
